b9411a7235716fd8a011841db4aadcab3f22206f
unknown <panditdipesh12@gmail.com>
2024-09-25 22:10:07 -0500

    updated readme

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import Homepage from "./Components/Pages/Homepage";
 import Publication from "./Components/Pages/Publication";
 import Teams from "./Components/Pages/Teams";
-import RootLayout from './Layout/RootLayout';
-import { Routes, Route} from 'react-router-dom';
+import RootLayout from "./Layout/RootLayout";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <RootLayout>
       <Routes>
-        <Route path="/" element={<Homepage />}/>
-        <Route path="/publication" element={<Publication />}/>
-        <Route path="/teams" element={< Teams />}/>
+        <Route path="" element={<Homepage />} />
+        <Route path="publication" element={<Publication />} />
+        <Route path="teams" element={<Teams />} />
       </Routes>
     </RootLayout>
   );
